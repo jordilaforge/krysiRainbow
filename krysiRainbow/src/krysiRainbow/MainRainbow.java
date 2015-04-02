06ddebdba090736385f5f9bb370e815694471d2f
mambax <dmnkmyr@gmail.com>
2015-04-02 10:30:44 +0200

    search for possible keys implemented, not yet correct working

--- krysiRainbow/src/krysiRainbow/MainRainbow.java
+++ krysiRainbow/src/krysiRainbow/MainRainbow.java
@@ -5,7 +5,7 @@ public class MainRainbow {
 	public static void main(String[] args) {
 		Rainbow a = new Rainbow();
 		a.populateRainbow();
-		a.printRainbow();
+		//a.printRainbow();
+		a.searchFor("1d56a37fb6b08aa709fe90e12ca59e12");
 	}
-
 }
--- krysiRainbow/src/krysiRainbow/Rainbow.java
+++ krysiRainbow/src/krysiRainbow/Rainbow.java
@@ -4,8 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 public class Rainbow {
@@ -14,21 +13,21 @@ public class Rainbow {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    private final String HASH = "1d56a37fb6b08aa709fe90e12ca59e12";
     private final int CHAINLENGTH = 2000;
     private final int WORDLENGTH = 7;
-    
-    public HashMap<String,String> rainbow;
-    
+
+    public HashMap<String, String> rainbow;
+
     /**
      * constructor for class rainbow
      */
-    public Rainbow(){
-    	rainbow = new HashMap<String,String>();
+    public Rainbow() {
+        rainbow = new HashMap<>();
     }
-    
+
     /**
      * MD5 hash function
+     *
      * @param r String to hash
      * @return hashed String
      */
@@ -53,16 +52,17 @@ public class Rainbow {
 
         return hexString.toString();
     }
-    
+
     /**
      * reduction function
-     * @param h String for reduction
+     *
+     * @param h     String for reduction
      * @param level level of reduction
      * @return
      */
     public String R(String h, int level) {
         BigInteger H = new BigInteger(h, 16);
-        H=H.add(BigInteger.valueOf(level));
+        H = H.add(BigInteger.valueOf(level));
         StringBuilder r = new StringBuilder();
 
         for (int i = 1; i <= WORDLENGTH; i++) {
@@ -76,60 +76,61 @@ public class Rainbow {
     /**
      * test method only for testing purpose
      */
-	public void VogtTest() {
-		String h1 = H("0000000");
-		System.out.println(h1);
-		String r1 = R(h1,0);
-		System.out.println(r1);
-		String h2 = H(r1);
-		System.out.println(h2);
-		String r2 = R(h2,1);
-		System.out.println(r2);
-		String h3 = H(r2);
-		System.out.println(h3);
-		String r3 = R(h3,2);
-		System.out.println(r3);
-		String h4 = H(r3);
-		System.out.println(h4);
-	}
-	
-	/**
-	 * populates rainbowtable witch possible Strings and their hashes
-	 */
-	public void populateRainbow(){
-		possibleStrings(WORDLENGTH,alphabet,"");
-		hashValues();
-	}
-	
-	private void hashValues(){
+    public void VogtTest() {
+        String h1 = H("0000000");
+        System.out.println(h1);
+        String r1 = R(h1, 0);
+        System.out.println(r1);
+        String h2 = H(r1);
+        System.out.println(h2);
+        String r2 = R(h2, 1);
+        System.out.println(r2);
+        String h3 = H(r2);
+        System.out.println(h3);
+        String r3 = R(h3, 2);
+        System.out.println(r3);
+        String h4 = H(r3);
+        System.out.println(h4);
+    }
+
+    /**
+     * populates rainbowtable witch possible Strings and their hashes
+     */
+    public void populateRainbow() {
+        possibleStrings(WORDLENGTH, alphabet, "");
+        determineChainEndHashes();
+    }
+
+    private void determineChainEndHashes() {
         for (Map.Entry<String, String> curr : rainbow.entrySet()) {
             String temp = curr.getKey();
-            for (int i = 0; i < CHAINLENGTH/2; i++) {
+            for (int i = 0; i < CHAINLENGTH; i++) {
                 temp = H(temp);
                 temp = R(temp, i);
             }
             rainbow.put(curr.getKey(), temp);
         }
-	}
-	
-	/**
-	 * adds possible strings to hashmap
-	 * @param maxLength max wordlength
-	 * @param alphabet 
-	 * @param curr
-	 */
-    private void possibleStrings(int maxLength, char[] alphabet, String curr) {
-    	if (rainbow.size() >= CHAINLENGTH) return;
-        // If the current string has reached it's maximum length
-        if(curr.length() == maxLength) {
-            rainbow.put(curr,"");
+    }
 
-        // Else add each letter from the alphabet to new strings and process these new strings again
+    /**
+     * adds possible passwords strings to hashmap
+     *
+     * @param maxLength max wordlength
+     * @param alphabet
+     * @param curr
+     */
+    private void possibleStrings(int maxLength, char[] alphabet, String curr) {
+        if (rainbow.size() >= CHAINLENGTH) return;
+        // If the current string has reached it's maximum length
+        if (curr.length() == maxLength) {
+            rainbow.put(curr, "");
+
+            // Else add each letter from the alphabet to new strings and process these new strings again
         } else {
-            for(int i = 0; i < alphabet.length; i++) {
+            for (int i = 0; i < alphabet.length; i++) {
                 String oldCurr = curr;
                 curr += alphabet[i];
-                possibleStrings(maxLength,alphabet,curr);
+                possibleStrings(maxLength, alphabet, curr);
                 curr = oldCurr;
             }
         }
@@ -137,20 +138,73 @@ public class Rainbow {
 
     /**
      * return number of elements in rainbow table
+     *
      * @return
      */
-	public int count() {
-		return rainbow.size();
-	}
-	
-	/**
-	 * prints rainbow table
-	 */
-	public void printRainbow(){
-		for (Entry<String, String> entry : rainbow.entrySet()) {
-		    System.out.println(entry.getKey() + " | " + entry.getValue());
-		}
-		System.out.println(count()+ " elements are in rainbow table");
-	}
+    public int count() {
+        return rainbow.size();
+    }
+
+    /**
+     * prints rainbow table
+     */
+    public void printRainbow() {
+        for (Entry<String, String> entry : rainbow.entrySet()) {
+            System.out.println(entry.getKey() + " | " + entry.getValue());
+        }
+        System.out.println(count() + " elements are in rainbow table");
+    }
+
+    /**
+     * search the rainbow table for a given hash, trying reduction function from chainlength to 0
+     *
+     * @param inHash hash, which should be found in table
+     * @return list of possible rainbow chain begins
+     */
+    public ArrayList<String> searchFor(String inHash) {
+        ArrayList<String> possibleResults = new ArrayList<>();
+
+        for (int i = CHAINLENGTH; i >= 0; i--) {
+            //System.out.println("Checking level " + i);
+            String s = iterateReduction(inHash, i);
+            if (rainbowContainsValue(s).isPresent()) {
+                System.out.println("A key has been found.");
+            }
+        }
+
+        System.out.println("Search has ended with " + possibleResults.size() + " results.");
+        return possibleResults;
+    }
+
+    /**
+     * @param hash
+     * @param currentLevel
+     * @return
+     */
+    private String iterateReduction(String hash, int currentLevel) {
+        while (currentLevel < CHAINLENGTH) {
+            hash = R(hash, currentLevel);
+            hash = H(hash);
+            currentLevel++;
+        }
+        hash = R(hash, CHAINLENGTH);
+        return hash;
+    }
+
+    /**
+     * @param k
+     * @return if a value is present, the value
+     */
+    private Optional<ArrayList<String>> rainbowContainsValue(String k) {
+
+        ArrayList<String> hits = new ArrayList<>();
+        for (Map.Entry<String, String> entr : rainbow.entrySet()) {
+            if (entr.getValue().toLowerCase().equals(k.toLowerCase())) {
+                hits.add(entr.getKey());
+            }
+        }
+        if (hits.size() > 0) return Optional.of(hits);
+        else return Optional.empty();
+    }
 }
 
