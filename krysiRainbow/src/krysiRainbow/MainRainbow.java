b930d0390a08464e6ee9dea83b0a0bf11895270d
jordilaforge <8899ph@web.de>
2015-04-02 02:00:47 +0200

    initial files

---
+++ krysiRainbow/src/krysiRainbow/MainRainbow.java
@@ -0,0 +1,10 @@
+package krysiRainbow;
+
+public class MainRainbow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ krysiRainbow/src/krysiRainbow/Rainbow.java
@@ -0,0 +1,5 @@
+package krysiRainbow;
+
+public class Rainbow {
+
+}
